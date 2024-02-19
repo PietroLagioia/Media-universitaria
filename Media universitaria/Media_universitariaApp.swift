1c9bbed7f3955bf3e536595f09e99b194b26a9c5
PietroLagioia <133354833+PietroLagioia@users.noreply.github.com>
2024-02-19 19:03:27 +0100

    Initial Commit

---
+++ Media universitaria/Media_universitariaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Media_universitariaApp.swift
+//  Media universitaria
+//
+//  Created by Pietro Lagioia on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Media_universitariaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
